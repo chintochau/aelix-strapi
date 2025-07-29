df439bdaf7a2cd1b621cbc99effbf98c23e54b12
Jason Chau <what.chau@gmail.com>
2025-07-29 13:37:19 -0400

    update s3 url

--- config/env/development/plugins.ts
+++ config/env/development/plugins.ts
@@ -1,22 +1,22 @@
-  export default ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          baseUrl: env('MINIO_PUBLIC_URL', 'https://storage.aelix.ai/strapi-uploads'),
-          s3Options: {
-            credentials: {
-              accessKeyId: env('MINIO_ACCESS_KEY', 'minioadmin'),
-              secretAccessKey: env('MINIO_SECRET_KEY', 'Aa31093066'),
-            },
-            endpoint: env('MINIO_ENDPOINT', 'http://localhost:9000'),
-            region: env('MINIO_REGION', 'us-east-1'),
-            forcePathStyle: true,
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: env('MINIO_PUBLIC_URL', 'https://storage.aelix.ai/strapi-uploads'),
+        s3Options: {
+          credentials: {
+            accessKeyId: env('MINIO_ACCESS_KEY', 'minioadmin'),
+            secretAccessKey: env('MINIO_SECRET_KEY', 'Aa31093066'),
           },
-          params: {
-            Bucket: env('MINIO_BUCKET', 'strapi-uploads'),
-          },
+          endpoint: env('MINIO_ENDPOINT', 'https://storage.aelix.ai'),
+          region: env('MINIO_REGION', 'us-east-1'),
+          forcePathStyle: true,
+        },
+        params: {
+          Bucket: env('MINIO_BUCKET', 'strapi-uploads'),
         },
       },
     },
-  });
+  },
+});
