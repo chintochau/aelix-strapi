70e141f338bb3441e29abf1e2a93354b0ac3d861
Jason Chau <what.chau@gmail.com>
2025-07-29 13:41:33 -0400

    update the csp

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,19 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'https://storage.aelix.ai'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://storage.aelix.ai'],
+          'connect-src': ["'self'", 'https:', 'http:'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
